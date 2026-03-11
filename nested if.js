5e67efa0593e0a8f2d164a86f4a83909d4770b9b
joshna0825 <gandlurijoshna0825@gmail.com>
2026-03-11 16:11:17 +0530

    Add files via upload

---
+++ nested if.js
@@ -0,0 +1,11 @@
+let num = 8;
+
+if (num >= 0) {           
+    if (num % 2 === 0) {  
+        console.log("The number is Even");
+    } else {
+        console.log("The number is Odd");
+    }
+} else {
+    console.log("Please enter a positive number");
+}
